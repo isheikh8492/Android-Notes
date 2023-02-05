720a81d140ead9ff0943b676153bd5dc9b63e248
isheikh8492 <isheikh8492@gmail.com>
2023-02-04 21:25:50 -0600

    added OptionsMenu on top with 2 buttons

--- app/src/main/java/com/imaduddinsheikh/androidnotes/MainActivity.java
+++ app/src/main/java/com/imaduddinsheikh/androidnotes/MainActivity.java
@@ -1,14 +1,30 @@
 package com.imaduddinsheikh.androidnotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
