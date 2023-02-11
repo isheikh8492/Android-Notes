e74451d593bcd36f9866f5a60e9140cb1a95cf76
isheikh8492 <isheikh8492@gmail.com>
2023-02-10 18:21:13 -0600

    added Save Note & return back to mainActivity

--- app/src/main/java/com/imaduddinsheikh/androidnotes/AddNoteActivity.java
+++ app/src/main/java/com/imaduddinsheikh/androidnotes/AddNoteActivity.java
@@ -1,7 +1,11 @@
 package com.imaduddinsheikh.androidnotes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddNoteActivity extends AppCompatActivity {
@@ -10,6 +14,25 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_note);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.add_new_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuSave) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
